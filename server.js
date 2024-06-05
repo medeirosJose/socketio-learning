d37c08b98606efe9c5b023baa52cadc68c99bf27
medeirosJose <62633628+medeirosJose@users.noreply.github.com>
2024-06-05 19:38:37 -0300

    adding some comments

--- server.js
+++ server.js
@@ -3,19 +3,23 @@ const http = require("http");
 const socketIo = require("socket.io");
 const axios = require("axios");
 
+// cria o server node
 const app = express();
 const server = http.createServer(app);
+
+// cria uma instância do socket.io e passa o server como parâmetro
 const io = socketIo(server);
 
 let ultimaImagem = "";
 let usuarios = {};
 
-// Rota cachorro
+// Get na api do cachorro
 app.get("/buscarCachorro", async (req, res) => {
   try {
     const response = await axios.get("https://dog.ceo/api/breeds/image/random");
     ultimaImagem = response.data.message;
-    io.emit("novaImagem", ultimaImagem); // troca a imagem para todos os clientes
+    // troca a imagem para todos os clientes conectados
+    io.emit("novaImagem", ultimaImagem);
     res.sendStatus(200);
   } catch (error) {
     console.error("Erro ao buscar imagem de cachorro:", error);
@@ -23,13 +27,14 @@ app.get("/buscarCachorro", async (req, res) => {
   }
 });
 
-// Rota gato
+// Get na api do gato
 app.get("/buscarGato", async (req, res) => {
   try {
     const response = await axios.get(
       "https://api.thecatapi.com/v1/images/search"
     );
     ultimaImagem = response.data[0].url;
+    // troca a imagem para todos os clientes conectados
     io.emit("novaImagem", ultimaImagem);
     res.sendStatus(200);
   } catch (error) {
@@ -38,32 +43,18 @@ app.get("/buscarGato", async (req, res) => {
   }
 });
 
-// Rota para o chat
-app.get("/chat", (req, res) => {
-  res.sendFile(__dirname + "/chat.html");
-});
-
-// Rota para o canvas
-app.get("/canvas", (req, res) => {
-  res.sendFile(__dirname + "/canvas.html");
-});
-
-// rota para o extras
-app.get("/extras", (req, res) => {
-  res.sendFile(__dirname + "/extras.html");
-});
-
 // conexão de um novo usuário
 io.on("connection", (socket) => {
   const userId = socket.id;
   usuarios[userId] = true;
   atualizarUsuariosConectados();
 
-  // fica aguardando receber uma mensagem do cliente (chat.html)
+  // fica aguardando receber uma mensagem do client (chat.html)
   socket.on("chatMessage", (message) => {
     io.emit("chatMessage", { userId, message });
   });
 
+  // fica aguardando a desconexão do client e atualiza a lista de usuarios conectados
   socket.on("disconnect", () => {
     delete usuarios[userId];
     atualizarUsuariosConectados();
@@ -89,6 +80,21 @@ function atualizarUsuariosConectados() {
 // rota para o index (página inicial)
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+// Rota para o chat
+app.get("/chat", (req, res) => {
+  res.sendFile(__dirname + "/chat.html");
+});
+
+// Rota para o canvas
+app.get("/canvas", (req, res) => {
+  res.sendFile(__dirname + "/canvas.html");
+});
+
+// rota para o extras
+app.get("/extras", (req, res) => {
+  res.sendFile(__dirname + "/extras.html");
 });
 
 // inicia o servidor na porta 3000
